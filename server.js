3d5a1497e120797843d554cde1296f7826ea9ec0
Timothy Strimple <tstrimple@gmail.com>
2013-11-08 17:26:03 -0800

    Initial express scaffolding

--- server.js
+++ server.js
@@ -1,26 +1,33 @@
 // https://github.com/nko4/website/blob/master/module/README.md#nodejs-knockout-deploy-check-ins
 require('nko')('vwPITUrcgKik0DFV');
 
-var isProduction = (process.env.NODE_ENV === 'production');
-var http = require('http');
-var port = (isProduction ? 80 : 8000);
+var http = require('http'),
+    path = require('path'),
+    isProduction = (process.env.NODE_ENV === 'production'),
+    port = (isProduction ? 80 : 8000),
+    express = require('express'),
+    app = express();
 
-http.createServer(function (req, res) {
-  // http://blog.nodeknockout.com/post/35364532732/protip-add-the-vote-ko-badge-to-your-app
-  var voteko = '<iframe src="http://nodeknockout.com/iframe/west-coast-hackers" frameborder=0 scrolling=no allowtransparency=true width=115 height=25></iframe>';
+app.set('port', port);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
 
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('<html><body>' + voteko + '</body></html>\n');
-}).listen(port, function(err) {
-  if (err) { console.error(err); process.exit(-1); }
+app.get('/', function(req, res) {
+  res.render('index');
+});
 
-  // if run as root, downgrade to the owner of this file
-  if (process.getuid() === 0) {
-    require('fs').stat(__filename, function(err, stats) {
-      if (err) { return console.error(err); }
-      process.setuid(stats.uid);
-    });
-  }
+app.get('/game', function(req, res) {
+  res.render('game');
+});
 
-  console.log('Server running at http://0.0.0.0:' + port + '/');
+var server = http.createServer(app);
+
+server.listen(app.get('port'), function() {
+  console.log('Express server listening on port ' + app.get('port'));
 });
