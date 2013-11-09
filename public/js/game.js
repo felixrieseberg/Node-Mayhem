6eb717e122b97016a3a07a3b2652e9553d7412a7
Felix Rieseberg <felix.rieseberg@microsoft.com>
2013-11-09 00:22:47 -0800

    Bullets!

--- public/js/game.js
+++ public/js/game.js
@@ -1,55 +1,78 @@
+
+/* Game namespace */
 var game = {
-  data: {
-      score: 0
-  },
 
-  'onload': function () {
-      me.sys.pauseOnBlur = false;
-      if (!me.video.init('screen', 1200, 800, true, 'auto')) {
-          alert('Your browser does not support HTML5 canvas.');
-          return;
-      }
-      // add '#debug' to the URL to enable the debug Panel
-      if (document.location.hash === '#debug') {
-          window.onReady(function () {
-              me.plugin.register.defer(debugPanel, 'debug');
-          });
-      }
+    // an object where to store game information
+    data: {
+        // score
+        score: 0
+    },
 
-      me.audio.init('mp3,ogg');
-      me.loader.onload = this.loaded.bind(this);
-      me.loader.preload(game.resources);
-      me.state.change(me.state.LOADING);
-  },
 
-  'loaded': function () {
-    me.state.set(me.state.MENU, new game.TitleScreen());
-    me.state.set(me.state.PLAY, new game.PlayScreen());
-    me.debug.renderHitBox = true;
-    me.entityPool.add('mainPlayer', game.PlayerEntity);
-    me.entityPool.add('bullet', game.BulletEntity, true);
+    // Run on page load.
+    "onload": function () {
+        // Initialize the video.
+        me.sys.pauseOnBlur = false;
+        if (!me.video.init("screen", 900, 700, true, 'auto')) {
+            alert("Your browser does not support HTML5 canvas.");
+            return;
+        }
+        // add "#debug" to the URL to enable the debug Panel
+        if (document.location.hash === "#debug") {
+            window.onReady(function () {
+                me.plugin.register.defer(debugPanel, "debug");
+            });
+        }
 
-    me.input.bindKey(me.input.KEY.SPACE, 'shoot');
-    me.input.bindMouse(0, me.input.KEY.SPACE);
-    me.input.bindKey(me.input.KEY.LEFT, 'left');
-    me.input.bindKey(me.input.KEY.A, 'left');
-    me.input.bindKey(me.input.KEY.RIGHT, 'right');
-    me.input.bindKey(me.input.KEY.D, 'right');
-    me.input.bindKey(me.input.KEY.UP, 'up');
-    me.input.bindKey(me.input.KEY.W, 'up');
-    me.input.bindKey(me.input.KEY.DOWN, 'down');
-    me.input.bindKey(me.input.KEY.S, 'down');
+        // Initialize the audio.
+        me.audio.init("mp3,ogg");
 
-    me.state.change(me.state.PLAY);
+        // Set a callback to run when loading is complete.
+        me.loader.onload = this.loaded.bind(this);
 
-    game.mp = new Multiplayer(function (x, y) {
-      var obj = me.entityPool.newInstanceOf('mainPlayer', x, y, {
-        spritewidth: 48,
-        spriteheight: 48,
-        isMP: true
-      });
-      me.game.add(obj, 1);
-      me.game.sort(); return obj;
-    });
-  }
+        // Load the resources.
+        me.loader.preload(game.resources);
+
+        // Initialize melonJS and display a loading screen.
+        me.state.change(me.state.LOADING);
+    },
+
+    // Run on game resources loaded.
+    "loaded": function () {
+        me.state.set(me.state.MENU, new game.TitleScreen());
+        me.state.set(me.state.PLAY, new game.PlayScreen());
+
+        // debug
+        me.debug.renderHitBox = true;
+
+        // add our player entity in the entity pool
+        me.entityPool.add("mainPlayer", game.PlayerEntity);
+        me.entityPool.add("bullet", game.BulletEntity, true);
+
+
+        // enable the keyboard
+        me.input.bindKey(me.input.KEY.SPACE, "shoot");
+        // map the left button click on the X key
+        me.input.bindMouse(0, me.input.KEY.SPACE);
+
+        me.input.bindKey(me.input.KEY.LEFT, "left");
+        me.input.bindKey(me.input.KEY.RIGHT, "right");
+        me.input.bindKey(me.input.KEY.UP, "up");
+        me.input.bindKey(me.input.KEY.DOWN, "down");
+        me.input.bindKey(me.input.KEY.X, "jump", true);
+
+        // Start the game.
+        me.state.change(me.state.PLAY);
+
+        game.mp = new Multiplayer(function (x, y) {
+            // Create a new player object 
+            var obj = me.entityPool.newInstanceOf("mainPlayer", x, y, {
+                spritewidth: 48,
+                spriteheight: 48,
+                isMP: true
+            });
+            me.game.add(obj, 1);
+            me.game.sort(); return obj;
+        });
+    }
 };
