abcba91f1bfd444697498fc2f6b077109853f8c9
Felix Rieseberg <felix.rieseberg@microsoft.com>
2013-11-10 00:31:31 -0800

    Resources & new map

--- public/js/resources.js
+++ public/js/resources.js
@@ -2,15 +2,18 @@ game.resources = [
   { name: "ground", type: "image", src: "data/img/ground.png" },
   { name: "rock", type: "image", src: "data/img/rock.png" },
   { name: "collision-obj", type: "image", src: "data/img/collision-obj.png" },
-  { name: "gripe_run_right", type: "image", src: "data/img/gripe_run_right.png" },
   { name: "boy", type: "image", src: "data/img/boy.png" },
   { name: "girl", type: "image", src: "data/img/girl.png" },
   { name: "bullet", type: "image", src: "data/img/bullet.png" },
-  { name: "tileset", type: "image", src: "data/img/tileset.png" },
-  { name: "testmap", type: "tmx", src: "data/map/map.tmx" },
   { name: "crate", type: "image", src: "data/img/crate.png" },
   { name: "gun", type: "image", src: "data/img/gun.png" },
   { name: "medpack", type: "image", src: "data/img/medpack.png" },
+
+  /* Map */
+  { name: "testmap", type: "tmx", src: "data/map/map.tmx" },
+  { name: "middleearth", type: "tmx", src: "data/map/middleearth.tmx" },
+  { name: "tilemap", type: "image", src: "data/map/tilemap.png" },
+  { name: "wood_tileset_3", type: "image", src: "data/img/wood_tileset_3.png" },
 
   /* Audio files */
   {name: "backgroundmusic1", type: "audio", src: "data/bgm/", channel: 2 },
