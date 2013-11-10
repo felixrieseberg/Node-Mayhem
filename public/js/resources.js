a9f66a5e07c6f46b7143280f2d66f71582e9e78b
Felix Rieseberg <felix.rieseberg@microsoft.com>
2013-11-09 17:54:46 -0800

    Music!

--- public/js/resources.js
+++ public/js/resources.js
@@ -1,13 +1,16 @@
 game.resources = [
-  {name: "ground",  type:"image", src: "data/img/ground.png"},
-  {name: "rock",  type:"image", src: "data/img/rock.png"},
-  {name: "collision-obj",  type:"image", src: "data/img/collision-obj.png"},
-  {name: "gripe_run_right", type:"image", src: "data/img/gripe_run_right.png"},
-  {name: "boy", type:"image", src: "data/img/boy.png"},
-  {name: "girl", type:"image", src: "data/img/girl.png"},
-  {name: "bullet", type:"image", src: "data/img/bullet.png"},
-  {name: "tileset", type:"image", src: "data/img/tileset.png"},
-  {name: "testmap", type: "tmx", src: "data/map/map.tmx"},
-  {name: "crate",  type:"image", src: "data/img/crate.png"},
-  {name: "gun", type:"image", src: "data/img/gun.png"}
+  { name: "ground", type: "image", src: "data/img/ground.png" },
+  { name: "rock", type: "image", src: "data/img/rock.png" },
+  { name: "collision-obj", type: "image", src: "data/img/collision-obj.png" },
+  { name: "gripe_run_right", type: "image", src: "data/img/gripe_run_right.png" },
+  { name: "boy", type: "image", src: "data/img/boy.png" },
+  { name: "girl", type: "image", src: "data/img/girl.png" },
+  { name: "bullet", type: "image", src: "data/img/bullet.png" },
+  { name: "tileset", type: "image", src: "data/img/tileset.png" },
+  { name: "testmap", type: "tmx", src: "data/map/map.tmx" },
+  { name: "crate", type: "image", src: "data/img/crate.png" },
+  { name: "gun", type: "image", src: "data/img/gun.png" },
+
+  /* Audio files */
+  {name: "backgroundmusic1", type: "audio", src: "data/bgm/", channel: 2 }
 ];
