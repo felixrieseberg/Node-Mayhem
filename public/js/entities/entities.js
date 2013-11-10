534a037f2d26ac79ae7ceff39ccba5ea874ed44c
Helen Zeng <helen.w.zeng@gmail.com>
2013-11-09 21:19:18 -0800

    adding hit animation

--- public/js/entities/entities.js
+++ public/js/entities/entities.js
@@ -13,6 +13,7 @@ game.NetworkPlayerEntity = me.CollectableEntity.extend({
         this.renderable.addAnimation('run-left', [1,5,9,13], 1);
         this.renderable.addAnimation('run-up', [2,6,10,14], 1);
         this.renderable.addAnimation('run-right', [3,7,11,15], 1);
+        this.renderable.addAnimation('hit', [0,1,2,3], 1);
         this.renderable.setCurrentAnimation('run-down');
         // set the default horizontal & vertical speed (accel vector)
         this.setVelocity(4, 4);
@@ -74,6 +75,7 @@ game.PlayerEntity = me.ObjectEntity.extend({
     this.renderable.addAnimation('run-left', [4, 5, 6, 7], 1);
     this.renderable.addAnimation('run-up', [8, 9, 10, 11], 1);
     this.renderable.addAnimation('run-right', [12, 13, 14, 15], 1);
+    this.renderable.addAnimation('hit', [0,4,8,12], 1);
     this.renderable.setCurrentAnimation('run-down');
     // set the default horizontal & vertical speed (accel vector)
     this.setVelocity(4, 4);
