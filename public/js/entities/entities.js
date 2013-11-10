ab5f28b17a706787dc0c03d5e06e1160d0e43ba1
Helen Zeng <helen.w.zeng@gmail.com>
2013-11-10 03:44:33 -0800

    last commit (attempts are unfruitful)

--- public/js/entities/entities.js
+++ public/js/entities/entities.js
@@ -63,107 +63,115 @@ game.NetworkPlayerEntity = me.CollectableEntity.extend({
 });
 
 game.PlayerEntity = me.ObjectEntity.extend({
-    init: function (x, y, settings) {
-        this.parent(x, y, settings);
+  init: function (x, y, settings) {
+    this.parent(x, y, settings);
 
-        //bounding
-        this.updateColRect(13, 26, 12, 30);
-        this.gravity = 0;
-        this.isWeaponCoolDown = false;
-        this.weaponCoolDownTime = 500;
+    //bounding
+    this.updateColRect(13, 26, 12, 30);
+    this.gravity = 0;
+    this.isWeaponCoolDown = false;
+    this.weaponCoolDownTime = 500;
 
-        // set up health (top 3, bottom 0)
-        this.health = 3;
-        this.id = settings.id;
+    // set up health (top 3, bottom 0)
+    this.health = 3;
+    this.id = settings.id;
 
-        // set up multiplayer
-        this.isMP = settings.isMP;
-        this.step = 0;
+    // set up multiplayer
+    this.isMP = settings.isMP;
+    this.step = 0;
 
-        // set up mouseCoordinates
-        game.mouseTarget = { x: 0, y: 0 };
+    // set up mouseCoordinates
+    game.mouseTarget = { x: 0, y: 0 };
 
-        this.isCollidable = true;
-        this.type = game.MAIN_PLAYER_OBJECT;
-        this.state = {};
+    this.isCollidable = true;
+    this.type = game.MAIN_PLAYER_OBJECT;
+    this.state = {};
 
-        this.renderable.addAnimation('run-down', [0, 1, 2, 3], 1);
-        this.renderable.addAnimation('run-left', [4, 5, 6, 7], 1);
-        this.renderable.addAnimation('run-up', [8, 9, 10, 11], 1);
-        this.renderable.addAnimation('run-right', [12, 13, 14, 15], 1);
-        this.renderable.addAnimation('hit', [0, 4, 8, 12], 1);
-        this.renderable.setCurrentAnimation('run-down');
-        // set the default horizontal & vertical speed (accel vector)
-        this.setVelocity(4, 4);
+    this.renderable.addAnimation('run-down', [0, 1, 2, 3], 1);
+    this.renderable.addAnimation('run-left', [4, 5, 6, 7], 1);
+    this.renderable.addAnimation('run-up', [8, 9, 10, 11], 1);
+    this.renderable.addAnimation('run-right', [12, 13, 14, 15], 1);
+    this.renderable.addAnimation('hit', [0, 4, 8, 12], 1);
+    this.renderable.setCurrentAnimation('run-down');
+    // set the default horizontal & vertical speed (accel vector)
+    this.setVelocity(4, 4);
 
-        me.game.viewport.follow(this.pos, me.game.viewport.AXIS.BOTH);
-    },
-    update: function () {
-        this.vel.x = 0;
-        this.vel.y = 0;
+    me.game.viewport.follow(this.pos, me.game.viewport.AXIS.BOTH);
+  },
+  update: function () {
+    this.vel.x = 0;
+    this.vel.y = 0;
 
-        if(this.state['ghost']) {
-            var ghost = this;
-            ghost.renderable.alpha = 0.25;
-            ghost.invincible = true;
-            setTimeout(function() { 
-                ghost.renderable.alpha = 1; 
-                ghost.invincible = false; 
-            }, 1500);
-        }
-
-
-        if (me.input.isKeyPressed('shoot')) {
-            if (!this.isWeaponCoolDown && me.input.isKeyPressed('shoot')) {
-                this.isWeaponCoolDown = true;
-                var player = this;
-
-                audioManager.playSound("shoot");
-
-                setTimeout(function () { player.isWeaponCoolDown = false; }, this.weaponCoolDownTime);
-                game.fireBullet({ x: this.pos.x + 12, y: this.pos.y + 12 }, game.mouseTarget, game.playerId, true);
-            }
-        }
-
-        var stateChanged = false;
-        var state = {};
-        if (me.input.isKeyPressed('left')) {
-            this.renderable.setCurrentAnimation('run-left');
-            this.vel.x -= this.accel.x * me.timer.tick;
-            state['left'] = true;
-            stateChanged = true;
-        }
-
-        if (me.input.isKeyPressed('right')) {
-            this.renderable.setCurrentAnimation('run-right');
-            this.vel.x += this.accel.x * me.timer.tick;
-            state['right'] = true;
-            stateChanged = true;
-        }
-
-        if (me.input.isKeyPressed('up')) {
-            this.renderable.setCurrentAnimation('run-up');
-            this.vel.y = -this.accel.y * me.timer.tick;
-            state['up'] = true;
-            stateChanged = true;
-        }
-
-        if (me.input.isKeyPressed('down')) {
-            this.renderable.setCurrentAnimation('run-down');
-            this.vel.y = this.accel.y * me.timer.tick;
-            state['down'] = true;
-            stateChanged = true;
-        }
-
-        this.updateMovement();
-
-        if (stateChanged) {
-            game.socket.emit('updatePlayerState', { x: this.pos.x, y: this.pos.y }, state);
-        }
-        this.state = {};
-        //check for collisions
-        var res = me.game.collide(this);
-
-        return true;
+    if (this.state['ghost']) {
+      var ghost = this;
+      ghost.renderable.alpha = 0.25;
+      ghost.invincible = true;
+      setTimeout(function () {
+        ghost.renderable.alpha = 1;
+        ghost.invincible = false;
+      }, 1500);
     }
+
+
+    if (me.input.isKeyPressed('shoot')) {
+      if (!this.isWeaponCoolDown && me.input.isKeyPressed('shoot')) {
+        this.isWeaponCoolDown = true;
+        var player = this;
+
+        audioManager.playSound("shoot");
+
+        setTimeout(function () { player.isWeaponCoolDown = false; }, this.weaponCoolDownTime);
+        game.fireBullet({ x: this.pos.x + 12, y: this.pos.y + 12 }, game.mouseTarget, game.playerId, true);
+      }
+    }
+
+    var stateChanged = false;
+    var state = {};
+    if (me.input.isKeyPressed('left')) {
+      this.renderable.setCurrentAnimation('run-left');
+      this.vel.x -= this.accel.x * me.timer.tick;
+      state['left'] = true;
+      stateChanged = true;
+    }
+
+    if (me.input.isKeyPressed('right')) {
+      this.renderable.setCurrentAnimation('run-right');
+      this.vel.x += this.accel.x * me.timer.tick;
+      state['right'] = true;
+      stateChanged = true;
+    }
+
+    if (me.input.isKeyPressed('up')) {
+      this.renderable.setCurrentAnimation('run-up');
+      this.vel.y = -this.accel.y * me.timer.tick;
+      state['up'] = true;
+      stateChanged = true;
+    }
+
+    if (me.input.isKeyPressed('down')) {
+      this.renderable.setCurrentAnimation('run-down');
+      this.vel.y = this.accel.y * me.timer.tick;
+      state['down'] = true;
+      stateChanged = true;
+    }
+
+    this.updateMovement();
+
+    if (stateChanged) {
+      game.socket.emit('updatePlayerState', { x: this.pos.x, y: this.pos.y }, state);
+    }
+    this.state = {};
+    //check for collisions
+    var res = me.game.collide(this);
+    if (res) {
+      // if we collide with an crate
+      if (res.obj.name == "crateentity") {
+        console.log("bounce");
+        this.vel.x -= this.accel.x * me.timer.tick;
+        this.vel.y -= this.accel.y * me.timer.tick;
+      }
+    }
+
+    return true;
+  }
 });
